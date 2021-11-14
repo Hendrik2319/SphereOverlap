e5c47226b974490a488734f8f9d9dc55e3e6ab35
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-11-14 11:46:04 +0000

    JAVA: [Eclipse] - Projekt "SphereOverlap"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/SphereOverlap@6160 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/tools/sphereoverlap/SphereOverlap.java
+++ src/net/schwarzbaer/java/tools/sphereoverlap/SphereOverlap.java
@@ -7,18 +7,18 @@ public class SphereOverlap {
 	}
 	
 	SphereOverlap() {
-//		Point3d[] points1 = new Point3d[] { 
-//				new Point3d(  -374.04,     309.45,    -450.11 ),
-//				new Point3d(  6067.64,   18269.06,   25806.78 ),
-//				new Point3d(  36924.51,  17250.54,     100.49 ),
-//				new Point3d(  31152.43, -14553.81,  -27841.96 ),
-//				new Point3d( -18964.20, -32550.94,  -26076.28 ),
-//				new Point3d(  39803.03,   7282.90,   21970.95 ),
-//				new Point3d( -30000.08,  29999.87,   29999.98 ),
-//				new Point3d(  -6421.75,  14191.34,  -53819.49 ),
-//				new Point3d(  47744.67,  54773.03,  -13383.40 ),
-//				new Point3d( -24134.06, -17430.58,  -69033.38 ),
-//				new Point3d( -12446.87, -11794.27, -107519.69 )
+//		ConstPoint3D[] points1 = new ConstPoint3D[] { 
+//				new ConstPoint3D(  -374.04,     309.45,    -450.11 ),
+//				new ConstPoint3D(  6067.64,   18269.06,   25806.78 ),
+//				new ConstPoint3D(  36924.51,  17250.54,     100.49 ),
+//				new ConstPoint3D(  31152.43, -14553.81,  -27841.96 ),
+//				new ConstPoint3D( -18964.20, -32550.94,  -26076.28 ),
+//				new ConstPoint3D(  39803.03,   7282.90,   21970.95 ),
+//				new ConstPoint3D( -30000.08,  29999.87,   29999.98 ),
+//				new ConstPoint3D(  -6421.75,  14191.34,  -53819.49 ),
+//				new ConstPoint3D(  47744.67,  54773.03,  -13383.40 ),
+//				new ConstPoint3D( -24134.06, -17430.58,  -69033.38 ),
+//				new ConstPoint3D( -12446.87, -11794.27, -107519.69 )
 //		};
 	}
 
