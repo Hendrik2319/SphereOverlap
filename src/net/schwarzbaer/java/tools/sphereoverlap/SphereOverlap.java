6e70bfcf3cc145347bc883cda443fe946e10289e
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-11-14 10:51:17 +0000

    JAVA: [Eclipse] - Projekt "SphereOverlap"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/SphereOverlap@6152 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/tools/sphereoverlap/SphereOverlap.java
+++ src/net/schwarzbaer/java/tools/sphereoverlap/SphereOverlap.java
@@ -1,5 +1,30 @@
 package net.schwarzbaer.java.tools.sphereoverlap;
 
 public class SphereOverlap {
+	
+	public static void main(String[] args) {
+		new SphereOverlap().initialize();
+	}
+	
+	SphereOverlap() {
+//		Point3d[] points1 = new Point3d[] { 
+//				new Point3d(  -374.04,     309.45,    -450.11 ),
+//				new Point3d(  6067.64,   18269.06,   25806.78 ),
+//				new Point3d(  36924.51,  17250.54,     100.49 ),
+//				new Point3d(  31152.43, -14553.81,  -27841.96 ),
+//				new Point3d( -18964.20, -32550.94,  -26076.28 ),
+//				new Point3d(  39803.03,   7282.90,   21970.95 ),
+//				new Point3d( -30000.08,  29999.87,   29999.98 ),
+//				new Point3d(  -6421.75,  14191.34,  -53819.49 ),
+//				new Point3d(  47744.67,  54773.03,  -13383.40 ),
+//				new Point3d( -24134.06, -17430.58,  -69033.38 ),
+//				new Point3d( -12446.87, -11794.27, -107519.69 )
+//		};
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
